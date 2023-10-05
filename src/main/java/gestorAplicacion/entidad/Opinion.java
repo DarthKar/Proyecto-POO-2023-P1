ba946ffa1f42dcf23de6e096a348ba9c1cdcf97e
Miguel Angel Peña Marin <138605407+DarthKar@users.noreply.github.com>
2023-10-04 21:11:32 -0500

    Ajustes Opinion
    Creacion del atributo creador e imports para funcionalidad del atributo
    Setters y Getters de los atributos

--- src/main/java/gestorAplicacion/entidad/Opinion.java
+++ src/main/java/gestorAplicacion/entidad/Opinion.java
@@ -1,9 +1,12 @@
 package gestorAplicacion.entidad;
+import gestorAplicacion.entidad.usuario.tiposDeUsuario.comprador.Comprador;
 
 public class Opinion {
     protected String opinion;
     protected int valoracion;
+    protected Comprador creador;        // Creacion de atributo creador para poder diferenciar entre una opinion y otra, filtrar por edicion de opiniones
 
+   
     public Opinion(String opinion, int valoracion) {
         this.opinion = opinion;
         this.valoracion = valoracion;
@@ -16,4 +19,20 @@ public class Opinion {
     public int getValoracion() {
         return valoracion;
     }
+
+    public void setOpinion(String opinion) {
+        this.opinion = opinion;
+    }
+
+    public void setValoracion(int valoracion) {
+        this.valoracion = valoracion;
+    }
+     public void setCreador(Comprador creador) {
+        this.creador = creador;
+    }                                                             //setter y getter de creador
+     
+    public Comprador getCreador() {
+        return creador;
+    }
+
 }
