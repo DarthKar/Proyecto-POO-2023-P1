c0e38771a2fabc098550bead33ab1d8eabcd6bc3
Miguel Angel Peña Marin <138605407+DarthKar@users.noreply.github.com>
2023-10-04 21:08:19 -0500

    Ajuste Producto
    Ajustes a producto con listas para identificar compradores y reseñadores para la validacion de las opiniones
    Setters y Getters de los atributos creados

--- src/main/java/gestorAplicacion/entidad/producto/Producto.java
+++ src/main/java/gestorAplicacion/entidad/producto/Producto.java
@@ -1,8 +1,10 @@
 package gestorAplicacion.entidad.producto;
 
+import gestorAplicacion.entidad.usuario.tiposDeUsuario.comprador.Comprador;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Producto implements Serializable {
 
@@ -10,14 +12,31 @@ public class Producto implements Serializable {
     private String nombre;
     private Categoria categoria;
     private List<OpinionProducto> opinion;
+    private List<Comprador> compradores;
+    private List<Comprador> resenadores;
 
+ 
     public Producto(Long id, String nombre, Categoria categoria) {
         this.id = id;
         this.nombre = nombre;
         this.categoria = categoria;
         this.opinion = new ArrayList<>();
+        this.compradores= new ArrayList<>();
     }
-
+    
+    public void addOpinionProducto(OpinionProducto resena){
+        if(Objects.isNull(resena ))
+            return;                                                 // Creacion del metodo addOpinion como en la clase vendedor
+        opinion.add(resena);
+        
+    }
+    
+    public boolean existeResena(Comprador comprador){
+        return resenadores.contains(comprador);                  // Creacion del metodo ExisteResena que comprueba si ya hay una reseña
+   
+    }
+    
+   
     public Long getId() {
         return id;
     }
@@ -33,4 +52,41 @@ public class Producto implements Serializable {
     public List<OpinionProducto> getOpiniones() {
         return opinion;
     }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+    
+    public void setComprodores(List compradores){
+        this.compradores=compradores;
+    }
+    
+    public List<Comprador> getCompradores(){
+        return compradores;
+    }
+    
+
+    public List<Comprador> getResenadores() {
+        return resenadores;
+    }
+       public void setOpinion(List<OpinionProducto> opinion) {
+        this.opinion = opinion;
+    }
+
+    public void setCompradores(List<Comprador> compradores) {
+        this.compradores = compradores;
+    }
+
+    public void setResenadores(List<Comprador> resenadores) {
+        this.resenadores = resenadores;
+    }
+   
 }
