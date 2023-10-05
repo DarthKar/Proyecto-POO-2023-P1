043f1fc2db9a48de057ad9606c2cf8839b3c4a96
Miguel Angel Peña Marin <138605407+DarthKar@users.noreply.github.com>
2023-10-04 21:05:12 -0500

    Ajustes a Vendedor
    Setters y Getters para atributos que no los tenian, creacion de atributos necesarios para la comprobacion de las opiniones e imports para esos atributos

--- src/main/java/gestorAplicacion/entidad/usuario/tiposDeUsuario/vendedor/Vendedor.java
+++ src/main/java/gestorAplicacion/entidad/usuario/tiposDeUsuario/vendedor/Vendedor.java
@@ -1,6 +1,7 @@
 package gestorAplicacion.entidad.usuario.tiposDeUsuario.vendedor;
 
 import gestorAplicacion.entidad.usuario.Usuario;
+import gestorAplicacion.entidad.usuario.tiposDeUsuario.comprador.Comprador;
 import gestorAplicacion.entidad.usuario.tiposDeUsuario.vendedor.opinion.OpinionVendedor;
 
 import java.util.ArrayList;
@@ -11,11 +12,16 @@ public class Vendedor extends Usuario {
 
     private List<ProductoVendedor> productosVendedor;
     private List<OpinionVendedor> opinionesVendedor;
+    private List<Comprador> compradores;
+    private List<Comprador> resenadores;
+
 
     public Vendedor(long id, String nombre, String apellido, String correo) {
         super(id, nombre, apellido, correo);
         this.productosVendedor = new ArrayList<>();
         this.opinionesVendedor = new ArrayList<>();
+        this.compradores = new ArrayList<>(); // Nuevo array para verificar si el comprador que va a crear una opinion si ha comprado en esa tienda
+        
     }
 
     public List<ProductoVendedor> getProductoVendedor() {
@@ -37,4 +43,35 @@ public class Vendedor extends Usuario {
             return;
         opinionesVendedor.add(opinionVendedor);
     }
+    
+      public boolean existeResena(Comprador comprador){
+        return resenadores.contains(comprador);                  // Creacion del metodo ExisteResena que comprueba si ya hay una reseña
+   
+    }
+    
+    public List<Comprador> getCompradores(){
+        return compradores;                                 // Getter y setter para esas listas
+    }                                                                      
+
+    public void setProductosVendedor(List<ProductoVendedor> productosVendedor) {
+        this.productosVendedor = productosVendedor;
+    }
+
+    public void setOpinionesVendedor(List<OpinionVendedor> opinionesVendedor) {
+        this.opinionesVendedor = opinionesVendedor;
+    }
+
+    public void setCompradores(List<Comprador> compradores) {
+        this.compradores = compradores;
+    }
+    
+    public void setResenadores(List<Comprador> resenadores) {
+        this.resenadores = resenadores;
+    }
+
+    public List<Comprador> getResenadores() {
+        return resenadores;
+    }
+    
+    
 }
